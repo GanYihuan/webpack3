c816050d40fb98f2b424a3196efb24f9343151d2
GanYihuan <583520052@qq.com>
2018-08-02 09:12:54 +0800

    3-13 由浅入深 webpack - 处理 CSS - 提取 CSS

--- chapter3/3-13/webpack.config.js
+++ chapter3/3-13/webpack.config.js
@@ -19,15 +19,20 @@ module.exports = {
         fallback: {
           loader: 'style-loader',
           options: {
+            /* singleton(是否只使用一个 style 标签) */
             singleton: true,
+            /* transform(转化, 浏览下, 插入页面前, 根据不同浏览器配置不同样式) */
             transform: './css.transform.js'
           }
         },
         use: [{
             loader: 'css-loader',
             options: {
+              /* 是否压缩 */
               minimize: true,
+              /* 启用 css-modules */
               modules: true,
+              /* 定义编译出来的名称 */
               localIdentName: '[path][name]_[local]_[hash:base64:5]'
             }
           },
@@ -41,6 +46,7 @@ module.exports = {
   plugins: [
     /* 提取 css */
     new ExtractTextWebpackPlugin({
+      /* 提取出来的 css 文件名字 */
       filename: '[name].min.css',
       /* 指定提取 css 范围, 提取初始化 */
       allChunks: false
