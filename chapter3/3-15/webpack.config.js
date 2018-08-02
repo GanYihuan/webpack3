098e014b922158e58451da82aaf3101355564390
GanYihuan <583520052@qq.com>
2018-08-02 09:25:41 +0800

    3-15, 3-16 由浅入深 webpack - Tree-shaking - JS CSS Tree-shaking

--- chapter3/3-15/webpack.config.js
+++ chapter3/3-15/webpack.config.js
@@ -23,15 +23,20 @@ module.exports = {
           fallback: {
             loader: 'style-loader',
             options: {
+              /* singleton(是否只使用一个 style 标签) */
               singleton: true,
+              /* transform(转化, 浏览下, 插入页面前, 根据不同浏览器配置不同样式) */
               transform: './css.transform.js'
             }
           },
           use: [{
               loader: 'css-loader',
               options: {
+                /* 是否压缩 */
                 minimize: true,
+                /* 启用 css-modules */
                 modules: true,
+                /* 定义编译出来的名称 */
                 localIdentName: '[path][name]_[local]_[hash:base64:5]'
               }
             },
@@ -75,6 +80,7 @@ module.exports = {
       /* 指定提取css范围, 提取初始化 */
       allChunks: false
     }),
+    /* 放 ExtractTextWebpackPlugin 后面 */
     /* 去除多余的 css */
     new PurifyCss({
       paths: glob.sync([
