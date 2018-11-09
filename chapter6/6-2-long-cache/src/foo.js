2b15060c1eb0a8ebbd9bd4e8e88319217d4d1e2c
GanYihuan <583520052@qq.com>
2018-11-09 22:04:50 +0800

    format

---
+++ chapter6/6-2-long-cache/src/foo.js
@@ -0,0 +1,10 @@
+﻿import module from './module'
+
+// import('./async').then(function(a) {
+// 	console.log('async')
+// })
+import(/* webpackChunkName: 'async' */ './async').then(function(a) {
+	console.log('async')
+})
+
+console.log('foo.js !! ' + module)
---
+++ chapter6/6-2-long-cache/webpack.config.js
@@ -0,0 +1,25 @@
+﻿const webpack = require('webpack')
+const path = require('path')
+
+ module.exports = {
+	mode: 'production',
+	entry: {
+    main: './src/foo',
+    vendor: ['react']
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].[chunkhash].js'
+  },
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks: 'initial',
+      minChunks: Infinity
+    }
+  },
+  plugins: [
+    new webpack.NamedChunksPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
+}
