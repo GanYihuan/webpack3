8fdc046d7a02630c75dd416fba28cddd629ec54f
GanYihuan <583520052@qq.com>
2018-11-18 18:07:06 +0800

    format

--- chapter6/6-2-long-cache/webpack.config.mutil.js
+++ chapter6/6-2-long-cache/webpack.config.mutil.js
@@ -7,84 +7,84 @@ const path = require('path')
 const CleanWebpack = require('clean-webpack-plugin')
 
 const baseConfig = {
-	entry: {
+  entry: {
     react: ['react']
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'js/[name].[chunkhash].js'
-	},
-	optimization: {
-		splitChunks: {
-			name: 'vendor',
-			chunks: 'initial',
-			minChunks: Infinity
-		}
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ExtractTextWebpackPlguin.extract({
-					fallback: {
-						loader: 'style-loader'
-					},
-					use: [
-						{
-							loader: 'css-loader'
-						}
-					]
-				})
-			}
-		]
-	},
-	plugins: [
-		new webpack.NamedChunksPlugin(),
-		new webpack.NamedModulesPlugin(),
-		new CleanWebpack(path.resolve(__dirname, 'dist')),
-		new ExtractTextWebpackPlguin({
-			filename: 'css/[name].[hash].css'
-		})
-	]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].[chunkhash].js'
+  },
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks: 'initial',
+      minChunks: Infinity
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextWebpackPlguin.extract({
+          fallback: {
+            loader: 'style-loader'
+          },
+          use: [
+            {
+              loader: 'css-loader'
+            }
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new webpack.NamedChunksPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new CleanWebpack(path.resolve(__dirname, 'dist')),
+    new ExtractTextWebpackPlguin({
+      filename: 'css/[name].[hash].css'
+    })
+  ]
 }
 
-const generatePage = function({
-	title = '',
-	entry = '',
-	template = './src/index.html',
-	name = '',
-	chunks = []
+const generatePage = function ({
+  title = '',
+  entry = '',
+  template = './src/index.html',
+  name = '',
+  chunks = []
 } = {}) {
-	return {
-		entry,
-		plugins: [
-			new HtmlWebpackPlugin({
-				chunks,
-				template,
-				title,
-				filename: name + '.html'
-			})
-		]
-	}
+  return {
+    entry,
+    plugins: [
+      new HtmlWebpackPlugin({
+        chunks,
+        template,
+        title,
+        filename: name + '.html'
+      })
+    ]
+  }
 }
 
 const pages = [
-	generatePage({
-		title: 'page A',
-		entry: {
-			a: './src/pages/a'
-		},
-		name: 'a',
-		chunks: ['react', 'a']
-	}),
-	generatePage({
-		title: 'page B',
-		entry: {
-			b: './src/pages/b'
-		},
-		name: 'b',
-		chunks: ['react', 'b']
-	})
+  generatePage({
+    title: 'page A',
+    entry: {
+      a: './src/pages/a'
+    },
+    name: 'a',
+    chunks: ['react', 'a']
+  }),
+  generatePage({
+    title: 'page B',
+    entry: {
+      b: './src/pages/b'
+    },
+    name: 'b',
+    chunks: ['react', 'b']
+  })
 ]
 
 module.exports = pages.map(page => merge(baseConfig, page))
--- chapter6/6-2-long-cache/webpack.config.single.js
+++ chapter6/6-2-long-cache/webpack.config.single.js
@@ -6,83 +6,83 @@ const ExtractTextWebpackPlguin = require('extract-text-webpack-plugin')
 const path = require('path')
 
 const baseConfig = {
-	entry: {
-		react: ['react']
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'js/[name].[chunkhash].js'
-	},
-	optimization: {
-		splitChunks: {
-			name: 'vendor',
-			chunks: 'initial',
-			minSize: 30000,
-			minChunks: 2
-		}
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ExtractTextWebpackPlguin.extract({
-					fallback: {
-						loader: 'style-loader'
-					},
-					use: [
-						{
-							loader: 'css-loader'
-						}
-					]
-				})
-			}
-		]
-	},
-	plugins: [
-		new CleanWebpack(path.resolve(__dirname, 'dist')),
-		new ExtractTextWebpackPlguin({
-			filename: 'css/[name].[hash].css'
-		})
-	]
+  entry: {
+    react: ['react']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].[chunkhash].js'
+  },
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks: 'initial',
+      minSize: 30000,
+      minChunks: 2
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextWebpackPlguin.extract({
+          fallback: {
+            loader: 'style-loader'
+          },
+          use: [
+            {
+              loader: 'css-loader'
+            }
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpack(path.resolve(__dirname, 'dist')),
+    new ExtractTextWebpackPlguin({
+      filename: 'css/[name].[hash].css'
+    })
+  ]
 }
 
-const generatePage = function({
-	title = '',
-	entry = '',
-	template = './src/index.html',
-	name = '',
-	chunks = []
+const generatePage = function ({
+  title = '',
+  entry = '',
+  template = './src/index.html',
+  name = '',
+  chunks = []
 } = {}) {
-	return {
-		entry,
-		plugins: [
-			new HtmlWebpackPlugin({
-				chunks,
-				template,
-				title,
-				filename: name + '.html'
-			})
-		]
-	}
+  return {
+    entry,
+    plugins: [
+      new HtmlWebpackPlugin({
+        chunks,
+        template,
+        title,
+        filename: name + '.html'
+      })
+    ]
+  }
 }
 
 const pages = [
-	generatePage({
-		title: 'page A',
-		entry: {
-			a: './src/pages/a'
-		},
-		name: 'a',
-		chunks: ['react', 'a']
-	}),
-	generatePage({
-		title: 'page B',
-		entry: {
-			b: './src/pages/b'
-		},
-		name: 'b',
-		chunks: ['react', 'b']
-	})
+  generatePage({
+    title: 'page A',
+    entry: {
+      a: './src/pages/a'
+    },
+    name: 'a',
+    chunks: ['react', 'a']
+  }),
+  generatePage({
+    title: 'page B',
+    entry: {
+      b: './src/pages/b'
+    },
+    name: 'b',
+    chunks: ['react', 'b']
+  })
 ]
 
 module.exports = merge([baseConfig].concat(pages))
